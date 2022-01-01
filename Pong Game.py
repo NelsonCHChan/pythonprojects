2765012ba1e4812ce16995539528fabf31875136
Oliver Overend <ollyoverend10@gmail.com>
2022-01-01 10:17:34 +0000

    chore: reformat using Black

--- Pong Game.py
+++ Pong Game.py
@@ -8,7 +8,7 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 
-#Paddle A
+# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -17,7 +17,7 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-#Paddle B
+# Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -26,7 +26,7 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-#Ball
+# Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
