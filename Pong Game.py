70ca34a6eed7e9bac74a466bb4c4876f3c11b936
Nelson Chan <90690513+NelsonCHChan@users.noreply.github.com>
2022-01-04 17:59:35 +0000

    Update Pong Game.py

--- Pong Game.py
+++ Pong Game.py
@@ -34,6 +34,64 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
+
 # Main game loop
 while True:
     wn.update()
+    
+     # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+     ball.sety(290)
+     ball.dy *= -1
+
+    if ball.ycor() < -290 :
+     ball.sety(-290)
+     ball.dy *= -1
+
+    if ball.xcor() > 390:
+     ball.goto(0, 0)
+     ball.dx *= -1
+
+    if ball.xcor() < -390:
+     ball.goto(0, 0)
+     ball.dx *= -1
+
+ # Paddle and ball collisions
+if ball.xcor() > 340 and ball.xcor() <350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40) :
+ ball.setx(340)
+ ball.dx *= -1
+
+    
