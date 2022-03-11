3db37da97fbb3a142a542588f197ee8e26bbfb51
Nelson Chan <90690513+NelsonCHChan@users.noreply.github.com>
2022-03-11 16:49:15 +0000

    Update Nelson's Trivia.py

--- Nelson's Trivia.py
+++ Nelson's Trivia.py
@@ -8,32 +8,32 @@ total_q = 4
 if ans.lower() == "yes":
     print("winner winner chicken dinner")
 else:
-    print("you suck")
+    print("you lost")
 
 ans = input("1. What is my favourite japanese dish?")
 if ans.lower() == "katsu curry":
     score += 1
     print("correct")
 else:
-    print("you suck")
+    print("you lost")
 
 ans = input("2. What is 2 + 9 + 8 x 7?")
 if ans == str(67):
     score += 1
     print("correct")
 else:
-    print("you suck")
+    print("you lost")
 
 ans = input("3. What is better AWS or Azure?")
 if ans == "AWS":
     score += 1
     print("correct")
 else:
-    print("you suck")
+    print("you lost")
 
 ans = input("4. What is my dogs name?")
 if ans.lower() == "miso":
     score += 1
     print("correct")
 else:
-    print("you suck")
+    print("you lost")
